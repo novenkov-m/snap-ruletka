881054380aec09c6c42a88b80d06a3951bd62c2c
chelixx <mihailnovenkov@yandex.ru>
2026-04-29 08:45:59 +0300

    ;lkjhgfd

--- app/api/auth.py
+++ app/api/auth.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from ..database import get_db
 from ..schemas.schemas import UserRegister, Token, UserOut
 from ..services.auth_service import register, login, get_current_user
+from ..models.models import User
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
@@ -28,3 +29,10 @@ async def get_current_active_user(
     if not user:
         raise HTTPException(status_code=401, detail="Неавторизован")
     return user
+
+#--------------------------
+@router.get("/me", response_model=UserOut)
+async def read_users_me(
+    current_user: User = Depends(get_current_active_user)
+):
+    return current_user
